51dbef23119f9dce571eae53d4a3894816ef0a8d
Mohamad Alief Rizky Ramadhan <aliefm144@gmail.com>
2021-12-25 11:10:03 +0700

    fix the ui in mahasiswa.php

--- mahasiswa.php
+++ mahasiswa.php
@@ -10,6 +10,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/style.css">
     <title>Library Information System</title>
 </head>
 <body>
@@ -17,35 +18,41 @@
         <div class="row">
             <div class="col-md-3"></div>
             <div class="col-md">
-                <h2 class="text-center mt-4">Form Mahasiswa</h2>
-                <a href="" class="btn btn-primary mt-4">Tambah Data</a>
+                <h2 class="text-center mt-4">Data Mahasiswa</h2>
+                <div class="mhs_button mt-4">
+                    <a href="add_data.php" class="btn btn-primary">Tambah Data</a>
+                    <div class="logout">
+                        <a href="index.php" class="btn btn-danger badge mt-3">Logout</a>
+                    </div>
+                </div>
                 <table class="table mt-4">
                     <thead>
                         <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">First</th>
-                        <th scope="col">Last</th>
-                        <th scope="col">Handle</th>
+                            <th scope="col">#</th>
+                            <th scope="col">Nama</th>
+                            <th scope="col">NIM</th>
+                            <th scope="col">Action</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                        <th scope="row">1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                        </tr>
-                        <tr>
-                        <th scope="row">2</th>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
-                        </tr>
-                        <tr>
-                        <th scope="row">3</th>
-                        <td colspan="2">Larry the Bird</td>
-                        <td>@twitter</td>
-                        </tr>
+                        <?php 
+                            $data = $db -> query("SELECT * FROM mahasiswa");
+                            $number = 1;
+                            while($data_mahasiswa = $data -> fetch_array()){
+                                echo "
+                                    <tr>
+                                    <th scope='row'>$number</th>
+                                    <td>$data_mahasiswa[nama]</td>
+                                    <td>$data_mahasiswa[nim]</td>
+                                    <td>
+                                    <a class='btn btn-primary badge' href='edit.php?id=$data_mahasiswa[id_mahasiswa]'>Edit</a>
+                                        <a class='btn btn-danger badge'>Delete</a>
+                                    </td>
+                                    </tr>
+                                    ";
+                                $number++;
+                            }
+                            ?>
                     </tbody>
                 </table>
             </div>
