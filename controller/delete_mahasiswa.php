81711849b865dd709e285405f06b9aad804849d9
Mohamad Alief Rizky Ramadhan <aliefm144@gmail.com>
2021-12-25 18:01:39 +0700

    delete some code that less function

--- controller/delete_mahasiswa.php
+++ controller/delete_mahasiswa.php
@@ -1,8 +1,6 @@
 <?php 
     include "connection.php";
     $id = $_GET['id'];
-    $nim = $_POST['nim'];
-    $nama = $_POST['nama'];
     $db->query("DELETE FROM mahasiswa WHERE id_mahasiswa='$id'");
     header("Location:../mahasiswa.php");
 ?>
