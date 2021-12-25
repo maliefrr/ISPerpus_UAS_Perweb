f16102c98f09fbbd34e49f4154385c63c17c3a77
Mohamad Alief Rizky Ramadhan <aliefm144@gmail.com>
2021-12-25 14:32:28 +0700

    edit title

--- add_data.php
+++ add_data.php
@@ -10,7 +10,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <title>Belajar Pemrograman Web</title>
+    <title>Library Information System</title>
 </head>
 <body>
     <h3 class="text-center mt-4">Form Tambah Data Mahasiswa</h3>
